c991ffbc5c2f2177bb518c557e2ca2ebac7093d8
creeds-knight <apedoarthur21@gmail.com>
2023-10-18 06:48:02 -0700

    adding more logic to NULL values

--- printf__.c
+++ printf__.c
@@ -15,7 +15,7 @@ int _printf(const char *format, ...)
 
 	while (*format)
 	{
-		if (format == NULL)
+		if ((format == NULL) || ((format[0] == '%') && (format[1] == '\0')))
 			return (-1);
 		if (*format != '%')
 		{
