bcb1a1a86fc9469d710e519221e25101fe2205f8
Chris-Emma <chrisemmaofficial@gmail.com>
2023-10-18 10:09:49 +0300

    Update putchrr.c

--- putchrr.c
+++ putchrr.c
@@ -2,7 +2,7 @@
 
 /**
  * _putchrr - A function that writes a character to the standard output
- * @c: Character tobe written
+ * @c: This is the Character to be written
  * Return: 1 on success
  */
 int _putchrr(char c)
